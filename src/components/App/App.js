fd035ea538fd0d9ed9c671d6e0ba9ab754a558e5
Fabio <fabio.sarrasl@gmail.com>
2024-11-25 17:03:38 +0100

    add image and working on SearchBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
 import SearchBar from "../SearchBar/SearchBar";
+import styles from './App.module.css';
 const App = () => {
 
-	return (<div>
+	return (<div className={styles.main}>
       <SearchBar/>
   </div>);
 };

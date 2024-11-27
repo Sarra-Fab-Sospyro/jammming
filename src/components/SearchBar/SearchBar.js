6d54d7c0aa78dcbc9e54682923ac3bf5d265fa56
Fabio <fabio.sarrasl@gmail.com>
2024-11-27 16:44:31 +0100

    Add css style to SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,15 +1,20 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import  styles from './SearchBar.module.css';
+import styles from "./SearchBar.module.css";
 const SearchBar = ({ track }) => {
 	const search = <FontAwesomeIcon icon={faSearch} />;
 	return (
 		<div className={styles.mainDiv}>
 			<form id="form">
 				<label htmlFor="searchBar">Search your tracks</label>
-				<div>
-					<input id={styles.searchBar} type="text" placeholder="search tracks" value={track} />
+				<div className={styles.field}>
+					<input
+						id={styles.searchBar}
+						type="text"
+						placeholder="search tracks"
+						value={track}
+					/>
 					<button className={styles.button}>Search {search}</button>
 				</div>
 			</form>

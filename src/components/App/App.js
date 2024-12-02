786d2750b74ffaf7ffdb0ce1074b053419670982
Fabio <fabio.sarrasl@gmail.com>
2024-12-02 16:38:12 +0100

    fixed tracklist. Now Track has been result to be see in Tracklist.

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,21 +2,37 @@
 import React, { useEffect, useState } from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import Track from "../Track/Track";
+import Tracklist from "../Tracklist/Tracklist";
 import styles from "./App.module.css";
 const App = () => {
-
-	const [track, setTrack] = useState({
-		title: "Song Title",
-		artist: "Artist Name",
-		album: "Album Name",
-		id: "1",
-    img: "/assets/Mic drop-bro.svg"
-	});
+	const [tracklist, setTracklist] = useState([
+		{
+			title: "Song Title",
+			artist: "Artist Name",
+			album: "Album Name",
+			id: "1",
+			img: "/assets/Mic drop-bro.svg",
+		},
+		{
+			title: "Song Title",
+			artist: "Artist Name",
+			album: "Album Name",
+			id: "2",
+			img: "/assets/Mic drop-bro.svg",
+		},
+		{
+			title: "Song Title",
+			artist: "Artist Name",
+			album: "Album Name",
+			id: "3",
+			img: "/assets/Mic drop-bro.svg",
+		},
+	]);
 
 	return (
 		<div className={styles.main}>
 			<SearchBar />
-			<Track track={track} />
+			<Tracklist tracklist={tracklist}/>
 		</div>
 	);
 };
--- src/components/Tracklist/Tracklist.js
+++ src/components/Tracklist/Tracklist.js
@@ -1,10 +1,17 @@
 import React from "react";
+import Track from "../Track/Track";
+import styles from "./Tracklist.module.css";
 
-const Tracklist = ()=>{
-    return(
-        <>
-        </>
-    );
+const Tracklist = ({ tracklist }) => {
+	return (
+		<div className={styles.tracklist}>
+			{tracklist.map((track) => (
+				<li key={track.id} className={styles.listItem}>
+					<Track track={track} />
+				</li>
+			))}
+		</div>
+	);
 };
 
 export default Tracklist;
